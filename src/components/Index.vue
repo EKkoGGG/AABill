b75e4421375dda2fbc4b2f821d306e91a2c72918
JJ <330564052@qq.com>
2020-07-15 17:19:33 +0800

    完善token验证

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,7 +2,7 @@
   <div>
     <el-card shadow="never">
       <el-button type="primary" @click="creatNewRoomGet">创建账单房间</el-button>
-      <el-button type="primary">查找账单房间</el-button>
+      <el-button type="primary" @click="inRoomDialogVisible = true">进入账单房间</el-button>
     </el-card>
 
     <el-dialog title="创建账单房间" :visible.sync="roomDialogVisible" width="30%">
@@ -17,6 +17,21 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="roomDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="creatNewRoomPost">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <el-dialog title="进入账单房间" :visible.sync="inRoomDialogVisible" width="30%">
+      <el-form :model="inRoomForm" :rules="roomFormRules" ref="inRoomFormRef" label-width="100px">
+        <el-form-item label="账单房间号" prop="roomId">
+          <el-input v-model.number="inRoomForm.roomId"></el-input>
+        </el-form-item>
+        <el-form-item label="设置密码" prop="roomPwd">
+          <el-input v-model.number="inRoomForm.roomPwd"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="inRoomDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="inBillRoom">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -36,18 +51,38 @@ export default {
         }
       }
     };
+    var checkId = (rule, value, callback) => {
+      if (!Number.isInteger(value)) {
+        callback(new Error("请输入房间号"));
+      } else {
+        if (value.toString().length != 6) {
+          callback(new Error("必须为6位长度数字"));
+        } else {
+          callback();
+        }
+      }
+    };
     return {
       roomDialogVisible: false,
+      inRoomDialogVisible: false,
       roomForm: {
         roomId: 123456,
         roomPwd: 1234
       },
+      inRoomForm: {
+        roomId: null,
+        roomPwd: null
+      },
       roomFormRules: {
-        roomPwd: [{ validator: checkPwd, trigger: "blur" }]
+        roomPwd: [{ validator: checkPwd, trigger: "blur" }],
+        roomId: [{ validator: checkId, trigger: "blur" }]
       }
     };
   },
   methods: {
+    inBillRoom(){
+      this.inRoomDialogVisible = true;
+    },
     async creatNewRoomGet() {
       await this.$http.get("NewRoom").then(res => {
         // console.log(res);
@@ -56,6 +91,7 @@ export default {
         }
         this.roomForm = res.data;
         this.roomDialogVisible = true;
+         sessionStorage.setItem("token",res.data.token)
       });
     },
     async creatNewRoomPost() {
--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ import './assets/css/global.css'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://localhost:44324/api/Bill/'
-// axios.interceptors.request.use(config =>{
-//   //console.log(config)
-//   config.headers.Authorization = sessionStorage.getItem('token')
-//   return config
-// })
+axios.interceptors.request.use(config =>{
+  //console.log(config)
+  config.headers.Authorization = sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
